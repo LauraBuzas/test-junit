3e0f9a8cb91b1c2b452356d47ae35e05138819fe
laura-andrada-buzas <laura-andrada.buzas@microfocus.com>
2021-04-07 13:31:57 +0300

    abcd

---
+++ src/test/java/com/Tst.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.junit.Test;
+
+public class Tst {
+    @Test
+    public void a() {
+        System.out.println("a");
+    }
+
+    @Test
+    public void b() {
+        assert 1 == 2;
+    }
+
+}
